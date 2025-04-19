80d694f541cfe0df81bd62c32c55aca64842878e
Rosamund Mather <rsmnd.mthr@gmail.com>
2025-04-19 15:42:30 +0200

    Make changes to HomePage

--- app/home/models.py
+++ app/home/models.py
@@ -1,5 +1,15 @@
 from wagtail.models import Page
+from wagtail.contrib.routable_page.models import RoutablePageMixin
+from wagtail.fields import StreamField
+from wagtail.admin.panels import FieldPanel
+from blocks.index import BaseStreamBlock
 
 
-class HomePage(Page):
-    pass
+class HomePage(RoutablePageMixin, Page):
+    body = StreamField(
+        BaseStreamBlock(features=["bold", "italic", ]),
+        verbose_name="Page body",
+        blank=True,
+        use_json_field=True
+    )
+    content_panels = Page.content_panels + [FieldPanel("body"), ]
